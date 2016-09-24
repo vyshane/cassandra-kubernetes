c65a194c84fffc78e58038f8adc69adad01daf26
Vy-Shane Xie <shane@node.mu>
2016-09-24 08:05:37 +0800

    Added wait to allow Kubernetes time to add new pod to the cassandra service DNS.

--- image/custom-entrypoint.sh
+++ image/custom-entrypoint.sh
@@ -2,14 +2,18 @@
 #
 # Configure Cassandra seed nodes.
 
+sleep 5
+
 my_ip=$(hostname --ip-address)
 
 CASSANDRA_SEEDS=$(host $PEER_DISCOVERY_SERVICE | \
     grep -v "not found" | \
+    grep -v "connection timed out" | \
     grep -v $my_ip | \
     sort | \
-    head -2 | xargs | \
-    awk '{print $4}')
+    head -3 | \
+    awk '{print $4}' | \
+    xargs)
 
 if [ ! -z "$CASSANDRA_SEEDS" ]; then
     export CASSANDRA_SEEDS
